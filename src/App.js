730939b20d86141b42ff984d50677270a2387546
YuriiHoi <yuriyhoi05@gmail.com>
2022-09-19 12:38:11 +0300

    git init SpaceX

--- src/App.js
+++ src/App.js
@@ -1,40 +1,13 @@
 import './App.css';
-import {useEffect, useState} from "react";
-import {getUser, getUsers} from "./servies/UserServis";
-import UserComponent from "./components/UserComponent";
+
 ;
 
 function App() {
 
-  let [users, setUsers] =  useState([]);
-  let [chosenUser, setChosenUser] = useState(null);
-
-
-  useEffect(() => {
-    getUsers().then(value => setUsers(value.data));
-  }, [])
-
-  const chooseUser = (id) => {
-    getUser(id).then(value => setChosenUser(value.data));
-  }
-
 
   return (
       <div>
-        {chosenUser && (<div>
-          {chosenUser.id}
-          <br/>
-          {chosenUser.name}
-        </div>)}
-        <hr/>
-        {
-          users.map(value => <
-              UserComponent
-              key = {value.id}
-              item={value}
-              chooseUser = {chooseUser}
-          /> )
-        }
+
       </div>
   );
 }
